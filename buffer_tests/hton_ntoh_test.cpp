7cad3ae58f0fe9d20e3ea4e189b1400c54b29f4f
Eric Silk <eric.silk@ericsilk.com>
2022-12-05 09:38:29 -0600

    got the hton/ntoh working

--- buffer_tests/hton_ntoh_test.cpp
+++ buffer_tests/hton_ntoh_test.cpp
@@ -3,64 +3,97 @@
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
+#include <limits>
 #include <string>
 
-constexpr auto BYTE_SIZE = sizeof(std::byte);
+// We'll need to add some special handling if we end up on a system with bytes != 8 bits
+// (some DSP's, some TI MCU's, etc.)
+// Note also that while std::byte is effectively an alias of unsigned char, there is no defined
+// template specialization of numeric_limits and thus it reports 0 bits; hence, the use of unsigned
+// char rather than std::byte here
+constexpr auto BITS_PER_BYTE = std::numeric_limits<unsigned char>::digits;
+static_assert(BITS_PER_BYTE == 8);
+
+// From here: https://stackoverflow.com/a/13352059/4718836
+// with modifications (std::byte instead of uint8_t)
+template <class T>
+static inline T hton_any(const T& input) {
+    T                 output(0);
+    const std::size_t size = sizeof(input);
+    std::byte*        data = reinterpret_cast<std::byte*>(&output);
+
+    for (std::size_t i = 0; i < size; i++) {
+        data[i] =
+            std::byte(reinterpret_cast<const size_t&>(input) >> ((size - i - 1) * BITS_PER_BYTE));
+    }
+
+    return output;
+};
+
+// ntoh and hton are inverses, the naming is a convenience
+template <class T>
+static inline T ntoh_any(const T& input) {
+    return hton_any(input);
+};
 
 template <class T>
-static const inline std::array<std::byte, sizeof(T)> hton_bytes(const T input) {
-    static_assert(std::is_arithmetic_v<T>);
-    constexpr size_t size_of_T = sizeof(T);
+static inline T hton_any_stl(const T& input) {
+    const std::size_t           size = sizeof(input);
+    std::array<std::byte, size> data = {std::byte(0)};
 
-    std::array<std::byte, size_of_T> ret;
-    const auto                       cast_value = static_cast<size_t>(input);
-    for (std::size_t i = 0; i < size_of_T; i++) {
-        const auto shift_size    = (size_of_T - i - i) * BYTE_SIZE;
-        const auto shifted_value = cast_value >> shift_size;
-        ret[i]                   = static_cast<std::byte>(shifted_value);
+    for (std::size_t i = 0; i < size; i++) {
+        data[i] =
+            std::byte(reinterpret_cast<const size_t&>(input) >> ((size - i - 1) * BITS_PER_BYTE));
     }
 
-    return ret;
+    return *reinterpret_cast<T*>(data.data());
+};
+
+template <class T>
+static inline T ntoh_any_stl(const T& input) {
+    return hton_any_stl(input);
+};
+
+template <class T>
+static inline void test_endianness_conversion(const T& value) {
+    const T value_swapped = hton_any(value);
+    const T value_back    = ntoh_any(value_swapped);
+
+    std::cout << typeid(T).name() << ((value == value_back) ? ": Yes" : ": No") << ::std::endl;
 }
 
-template <class ReturnType, class BufferType>
-static const inline ReturnType ntoh_bytes(const BufferType& bytes) {
-    static_assert(std::is_arithmetic_v<ReturnType>);
-    constexpr size_t size_of_ret = sizeof(ReturnType);
-    assert(size_of_ret == bytes.size());
+template <class T>
+static inline void test_endianness_conversion_stl(const T& value) {
+    const T value_swapped = hton_any_stl(value);
+    const T value_back    = ntoh_any_stl(value_swapped);
 
-    size_t ret = 0;
-
-    for (std::size_t i = 0; i < size_of_ret; i++) {
-        // size_t to guarantee it will always fit after shifting
-        const auto cast_value    = static_cast<size_t>(bytes[i]);
-        const auto shift_size    = (size_of_ret - i - 1) * BYTE_SIZE;
-        const auto shifted_value = cast_value << (size_of_ret - i - 1) * BYTE_SIZE;
-        ret |= shifted_value;
-    }
-
-    return static_cast<ReturnType>(ret);
+    std::cout << typeid(T).name() << ((value == value_back) ? ": Yes" : ": No") << ::std::endl;
 }
 
 int main(void) {
-    const uint8_t a = 5;
-    const int     b = 123;
-    const float   c = 5463.5;
+    std::cout << "Raw C Arrays:" << std::endl;
+    test_endianness_conversion<uint8_t>(5);
+    test_endianness_conversion<uint8_t>(253);
 
-    auto          a_bytes = hton_bytes(a);
-    const uint8_t a_back  = ntoh_bytes<uint8_t>(a_bytes);
-    auto          b_bytes = hton_bytes(123);
-    const int     b_back  = ntoh_bytes<int>(b_bytes);
-    auto          c_bytes = hton_bytes(c);
-    const float   c_back  = ntoh_bytes<float>(c_bytes);
+    test_endianness_conversion<int>(2523);
 
+    test_endianness_conversion<float>(632642.54534);
+    test_endianness_conversion<float>(-632642.54534);
 
-    const std::string uint8_worked = (a == a_back) ? "Yes" : "No";
-    const std::string int_worked   = (a == a_back) ? "Yes" : "No";
-    const std::string float_worked = (a == a_back) ? "Yes" : "No";
-    std::cout << "uint8_t: " << uint8_worked << std::endl;
-    std::cout << "int: " << int_worked << std::endl;
-    std::cout << "float: " << float_worked << std::endl;
+    test_endianness_conversion<double>(5345235235.62834562);
+    test_endianness_conversion<double>(-5345235235.62834562);
+
+    std::cout << "STL Arrays:" << std::endl;
+    test_endianness_conversion_stl<uint8_t>(5);
+    test_endianness_conversion_stl<uint8_t>(253);
+
+    test_endianness_conversion_stl<int>(2523);
+
+    test_endianness_conversion_stl<float>(632642.54534);
+    test_endianness_conversion_stl<float>(-632642.54534);
+
+    test_endianness_conversion_stl<double>(5345235235.62834562);
+    test_endianness_conversion_stl<double>(-5345235235.62834562);
 
     return 0;
 }
